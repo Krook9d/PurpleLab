6536539e98810ca3c292900b90e2c87ef36311d8
Krook9d <40600995+Krook9d@users.noreply.github.com>
2023-10-30 19:22:33 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,6 +8,10 @@ apt-get update
 apt-get install -y apache2
 apt install -y php libapache2-mod-php
 apt-get install -y php-mysqli
+apt install -y python3-pip
+apt-get -y install p7zip-full
+pip install pandas
+
 phpenmod mysqli
 systemctl restart apache2
 apt install -y mysql-server
