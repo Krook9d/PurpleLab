f1c25d76a4dfda7ddb32b8de22445e99473a7411
Krook9d <40600995+Krook9d@users.noreply.github.com>
2024-01-22 11:24:31 +0100

    Update register.php

--- register.php
+++ register.php
@@ -7,14 +7,13 @@ $conn = new mysqli(
     getenv('DB_NAME')
 );
 
-// Vérification de la connexion
+
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-// Vérification que le formulaire a été soumis
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-    // Nettoyage et validation des entrées de l'utilisateur
+    // Cleaning and validation of user inputs
     $firstName = filter_input(INPUT_POST, 'firstName', FILTER_SANITIZE_STRING);
     $lastName = filter_input(INPUT_POST, 'lastName', FILTER_SANITIZE_STRING);
     $email = filter_input(INPUT_POST, 'email', FILTER_SANITIZE_EMAIL);
@@ -25,41 +24,37 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         die("Invalid email format");
     }
 
-    // Validation du mot de passe
+    // Password validation
     if (!preg_match("/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/", $password)) {
         die("Password must be at least 8 characters long and include at least one uppercase letter, one lowercase letter, one number, and one special character.");
     }
     $hashedPassword = password_hash($password, PASSWORD_DEFAULT);
 
-    // Vérification que le champ "avatar" existe
     if (isset($_FILES["avatar"])) {
-        // Upload de l'avatar
+
         $targetDir = "uploads/";
         $avatarFile = $targetDir . basename($_FILES["avatar"]["name"]);
         $imageFileType = strtolower(pathinfo($avatarFile, PATHINFO_EXTENSION));
 
-        // Vérifier si le fichier a été uploadé sans erreur
+        // Check that the file has been uploaded without errors
         if ($_FILES["avatar"]["error"] == UPLOAD_ERR_OK) {
-            // Vérifie si le fichier image est une image réelle ou une fausse image
+            // Checks whether the image file is a real image or a fake image
             $check = getimagesize($_FILES["avatar"]["tmp_name"]);
             if ($check !== false) {
-                // Autoriser certains formats de fichier
+                // Allow certain file formats
                 if ($imageFileType == "jpg" || $imageFileType == "png" || $imageFileType == "jpeg") {
-                    // Déplacer le fichier dans le dossier de destination
+                    // Move file to destination folder
                     if (move_uploaded_file($_FILES["avatar"]["tmp_name"], $avatarFile)) {
-                        // Utiliser des déclarations préparées pour éviter les injections SQL
+                        // Use prepared statements to avoid SQL injections
                         $stmt = $conn->prepare("INSERT INTO users (first_name, last_name, email, analyst_level, avatar, password) VALUES (?, ?, ?, ?, ?, ?)");
 
-                        // Vérifier si la préparation de la requête a réussi
                         if ($stmt === false) {
                             die("Error in prepared statement<br>");
                         }
 
                         $stmt->bind_param("ssssss", $firstName, $lastName, $email, $analystLevel, $avatarFile, $hashedPassword);
 
-                        // Exécuter la requête d'insertion
                         if ($stmt->execute()) {
-                            // Imprimer le code JavaScript pour afficher le popup
                             echo "<script type='text/javascript'>
                                     alert('User successfully created');
                                     if (confirm('User successfully created. Do you want to go to the login page?')) {
