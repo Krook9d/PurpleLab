7f98aaa9192a5b0df09e0f9ff6968dde16b78249
Krook9d <40600995+Krook9d@users.noreply.github.com>
2024-02-12 16:38:03 +0100

    Update manageVM.py

--- scripts/manageVM.py
+++ scripts/manageVM.py
@@ -27,7 +27,6 @@ def get_vm_ip():
     command = 'echo "IP = $(VBoxManage guestproperty enumerate "sandbox" | grep -oP \'/VirtualBox/GuestInfo/Net/0/V4/IP, value: \K[^,]+\')"'
     subprocess.run(command, shell=True)
 
-
 def upload_to_vm():
     # Command to copy files to the virtual machine
     command = (
@@ -35,6 +34,39 @@ def upload_to_vm():
         '--username oem --password oem --target-directory '
         '"C:\\Users\\oem\\Documents\\malware_upload" --recursive '
         '"/var/www/html/Downloaded/malware_upload"'
+    )
+    subprocess.run(command, shell=True)
+
+def disable_antivirus():
+# Command to disable Windows Defender real-time monitoring
+    powershell_command = (
+    'VBoxManage guestcontrol "sandbox" run --exe '
+    '"C:\\Windows\\System32\\WindowsPowerShell\\v1.0\\powershell.exe" --username oem --password oem '
+    '-- -Command "& {Start-Process powershell -ArgumentList \'Set-MpPreference -DisableRealtimeMonitoring \$true\' -Verb RunAs}"'
+)
+
+    # Run the command to disable Windows Defender real-time monitoring
+    subprocess.run(powershell_command, shell=True, check=True)
+    print("PowerShell command successfully executed on the VM.")
+
+def enable_antivirus():
+# Command to disable Windows Defender real-time monitoring
+    powershell_command = (
+    'VBoxManage guestcontrol "sandbox" run --exe '
+    '"C:\\Windows\\System32\\WindowsPowerShell\\v1.0\\powershell.exe" --username oem --password oem '
+    '-- -Command "& {Start-Process powershell -ArgumentList \'Set-MpPreference -DisableRealtimeMonitoring \$false\' -Verb RunAs}"'
+)
+
+    # Run the command to disable Windows Defender real-time monitoring
+    subprocess.run(powershell_command, shell=True, check=True)
+    print("PowerShell command successfully executed on the VM.")
+
+def restart_winlogbeat():
+    # Command to restart the winlogbeat service
+    command = (
+        'VBoxManage guestcontrol "sandbox" run --exe '
+        '"C:\\Windows\\System32\\WindowsPowerShell\\v1.0\\powershell.exe" --username oem --password oem '
+        '-- -Command "& {Start-Process powershell -ArgumentList \'Restart-Service winlogbeat\' -Verb RunAs}"'
     )
     subprocess.run(command, shell=True)
 
@@ -46,19 +78,24 @@ command_name = sys.argv[1]
 
 if command_name == "restore":
     poweroff_vm()
-
     restore_snapshot()
-   
     time.sleep(1)
-    
     start_vm_headless()
 elif command_name == "state":
-    # Display virtual machine information
     show_vm_info()
 elif command_name == "upload":
-    # Copy files to the virtual machine
     upload_to_vm()
 elif command_name == "ip":
     get_vm_ip()
+elif command_name == "poweroff":
+    poweroff_vm()
+elif command_name == "startheadless":
+    start_vm_headless()
+elif command_name == "disableav":
+    disable_antivirus()
+elif command_name == "enableav":
+    enable_antivirus()
+elif command_name == "restartwinlogbeat":
+    restart_winlogbeat()
 else:
     print("Command not recognized. Utilisation: python manageVM.py <commande>")
