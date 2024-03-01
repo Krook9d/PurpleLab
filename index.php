c138314036b5c3dac7ea5bd071b33c2faca33570
Krook9d <40600995+Krook9d@users.noreply.github.com>
2024-03-01 17:28:07 +0100

    Update index.php

--- index.php
+++ index.php
@@ -347,7 +347,7 @@ if (isset($responseData7['aggregations']['source_ips']['buckets'])) {
 <!DOCTYPE html>
 <html lang="fr">
 <head>
-    <link rel="icon" href="logo.png" type="image/png">
+    <link rel="icon" href="MD_image/logowhite.png" type="image/png">
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Purplelab</title>
