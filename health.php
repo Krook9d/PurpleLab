4a2195e1947d634401fbaa76e4d12e918d766527
Krook9d <40600995+Krook9d@users.noreply.github.com>
2024-03-17 20:20:58 +0100

    Update health.php

--- health.php
+++ health.php
@@ -188,7 +188,7 @@ $vmInfo['IP'] = $vmIP;
 <!DOCTYPE html>
 <html lang="fr">
 <head>
-    <link rel="MD_image/logowhite.png" href="logo.png" type="image/png">
+    <link rel="icon" href="MD_image/logowhite.png" type="image/png">
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Purplelab</title>
