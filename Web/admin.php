34e6ac1796ba5129cee2f42dbf21b790b8fa1195
Krook9d <40600995+Krook9d@users.noreply.github.com>
2024-06-21 14:41:37 +0200

    Update admin.php

--- Web/admin.php
+++ Web/admin.php
@@ -42,7 +42,7 @@ $stmt->close();
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Purplelab</title>
-    <link rel="stylesheet" href="styles.css?v=5.3">
+    <link rel="stylesheet" href="css/main.css?v=<?= filemtime('css/main.css') ?>">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     
