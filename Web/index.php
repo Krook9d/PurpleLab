b1cb7612cedf07cb6f538fd4eeb979965feb32d3
Krook9d <40600995+Krook9d@users.noreply.github.com>
2024-06-21 14:42:58 +0200

    Update index.php

--- Web/index.php
+++ Web/index.php
@@ -363,7 +363,7 @@ countYmlFiles($sigmaDirectory);
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Purplelab</title>
-    <link rel="stylesheet" href="styles.css?v=<?= filemtime('styles.css') ?>">
+    <link rel="stylesheet" href="css/main.css?v=<?= filemtime('css/main.css') ?>">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
     <script src="https://cdnjs.cloudflare.com/ajax/libs/raphael/2.3.0/raphael.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/justgage@1.3.2/dist/justgage.min.js"></script>
