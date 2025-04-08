c654cb95992e166963a3d45f89aaf8f03f4235a9
Krook9d <40600995+Krook9d@users.noreply.github.com>
2025-04-08 21:02:27 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -213,7 +213,7 @@ echo -e "\n\e[32mAll checks passed. Continuing with the installation...\e[0m\n"
     # Download the sandbox.ova file
     
     #curl -L -o sandbox.ova -H "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3" "https://dl-15ur2kes.swisstransfer.com/api/download/88c4eede-91db-4016-b8a6-57f7282dbab7/c367102d-17c1-4e5c-8161-3233f98c83b0"
-    curl -L -o sandbox.ova -H "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3" "https://dl-udx0dzf1.swisstransfer.com/api/download/c7973e3a-0456-460f-8f23-aa175a6b21c7/41a14b9d-40e9-4819-bb0f-e32f9ac91320"
+    curl -L -o sandbox.ova -H "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3" "https://dl-29vb9178.swisstransfer.com/api/download/8bde5d12-f669-4f7f-82da-36a4c9f806b3/9e6f02f9-dfef-4875-9807-32c20fd46f6b"
     
     mv sandbox.ova /var/www/html/
 
